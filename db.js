d93c8fcada81551edc1d4fd2f6eb100e1967c48e
Mounika Singampalli <134005998+mouni131199@users.noreply.github.com>
2025-12-16 17:31:15 +0530

    Fix start script for Railway

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
+
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Mouni@000",
-    database: "passport"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+
 const db = require("./db");
 
 const userRoutes = require("./routes/userRoutes");
